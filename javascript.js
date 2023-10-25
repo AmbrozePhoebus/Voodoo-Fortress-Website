4967ca74b4cc93f0fbfabe131cba5d0d0a8f2d4c
Orim12 <miro.vaassen@gmail.com>
2023-10-25 11:21:33 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,6 +0,0 @@
-var button = document.createElement("button");
-button.innerHTML = "Click me";
-
-<button onclick="window.location.href='https://www.example.com';">Go to example.com</button>
-
-document.body.appendChild(button);
